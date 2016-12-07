dc36060851c6b0712da9a72f477b6a80ad18f304
Laurens Offereins <lmoffereins@gmail.com>
2016-12-07 16:46:04 +0100

    Admin: introduce settings structure and admin tabs

--- includes/admin.php
+++ includes/admin.php
@@ -43,7 +43,8 @@ class Paco2017_Admin {
 	 * @since 1.0.0
 	 */
 	private function setup_actions() {
-		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'admin_menu',   array( $this, 'admin_menu'        )        );
+		add_action( 'admin_init',   array( $this, 'register_settings' )        );
 	}
 
 	/** Public methods **************************************************/
@@ -96,14 +97,18 @@ class Paco2017_Admin {
 		}
 
 		// Settings page
-		add_submenu_page(
-			'paco2017',
-			__( 'Paascongres 2017 Settings', 'paco2017-content' ),
-			__( 'Settings', 'paco2017-content' ),
-			'paco2017_admin_settings_page',
-			'paco2017-settings',
-			'paco2017_admin_settings_page'
-		);
+		if ( paco2017_admin_page_has_settings( 'paco2017' ) ) {
+			add_submenu_page(
+				'paco2017',
+				__( 'Paascongres 2017 Settings', 'paco2017-content' ),
+				__( 'Settings', 'paco2017-content' ),
+				'paco2017_admin_settings_page',
+				'paco2017-settings',
+				'paco2017_admin_page'
+			);
+
+			add_action( 'paco2017_admin_page-paco2017-settings', 'paco2017_admin_settings_page' );
+		}
 
 		foreach ( $hooks as $hook ) {
 			add_action( "admin_head-{$hook}", array( $this, 'admin_menu_highlight' ) );
@@ -126,6 +131,56 @@ class Paco2017_Admin {
 		if ( in_array( $post_type, array( paco2017_get_lector_post_type(), paco2017_get_workshop_post_type() ) ) ) {
 			$parent_file  = 'paco2017';
 			$submenu_file = "edit.php?post_type={$post_type}";
+		}
+	}
+
+	/**
+	 * Register plugin settings
+	 *
+	 * @since 1.0.0
+	 */
+	public function register_settings() {
+
+		// Bail if no sections available
+		$sections = paco2017_admin_get_settings_sections();
+		if ( empty( $sections ) )
+			return false;
+
+		// Loop through sections
+		foreach ( (array) $sections as $section_id => $section ) {
+
+			// Only proceed if current user can see this section
+			if ( ! current_user_can( $section_id ) )
+				continue;
+
+			// Only add section and fields if section has fields
+			$fields = paco2017_admin_get_settings_fields_for_section( $section_id );
+			if ( empty( $fields ) )
+				continue;
+
+			// Define section page
+			if ( ! empty( $section['page'] ) ) {
+				$page = $section['page'];
+			} else {
+				$page = 'paco2017';
+			}
+
+			// Add the section
+			add_settings_section( $section_id, $section['title'], $section['callback'], $page );
+
+			// Loop through fields for this section
+			foreach ( (array) $fields as $field_id => $field ) {
+
+				// Add the field
+				if ( ! empty( $field['callback'] ) && ! empty( $field['title'] ) ) {
+					add_settings_field( $field_id, $field['title'], $field['callback'], $page, $section_id, $field['args'] );
+				}
+
+				// Register the setting
+				if ( ! empty( $field['sanitize_callback'] ) ) {
+					register_setting( $page, $field_id, $field['sanitize_callback'] );
+				}
+			}
 		}
 	}
 }
--- includes/settings.php
+++ includes/settings.php
@@ -10,14 +10,125 @@
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
 
+/** Settings **************************************************************/
+
+/**
+ * Return admin settings sections
+ *
+ * @since 1.0.0
+ *
+ * @uses apply_filters() Calls 'paco2017_admin_get_settings_sections' with the sections
+ * @return array Settings sections
+ */
+function paco2017_admin_get_settings_sections() {
+	return (array) apply_filters( 'paco2017_admin_get_settings_sections', array(
+
+		// Main settings
+		'paco2017_settings_main' => array(
+			'title'    => __( 'Main Settings', 'paco2017' ),
+			'callback' => 'paco2017_admin_setting_callback_main_section',
+			'page'     => 'paco2017'
+		),
+	) );
+}
+
+/**
+ * Return admin settings fields
+ *
+ * @since 1.0.0
+ *
+ * @uses apply_filters() Calls 'paco2017_admin_get_settings_fields' with the fields
+ * @return array Settings fields
+ */
+function paco2017_admin_get_settings_fields() {
+	return (array) apply_filters( 'paco2017_admin_get_settings_fields', array(
+
+		/** Main Section **************************************************/
+
+		'paco2017_settings_main' => array(
+		),
+	) );
+}
+
+/**
+ * Get settings fields by section
+ *
+ * @since 1.0.0
+ *
+ * @param string $section_id Section id
+ * @return mixed False if section is invalid, array of fields otherwise
+ */
+function paco2017_admin_get_settings_fields_for_section( $section_id = '' ) {
+
+	// Bail if section is empty
+	if ( empty( $section_id ) )
+		return false;
+
+	$fields = paco2017_admin_get_settings_fields();
+	$retval = isset( $fields[$section_id] ) ? $fields[$section_id] : false;
+
+	return (array) apply_filters( 'paco2017_admin_get_settings_fields_for_section', $retval, $section_id );
+}
+
+/**
+ * Return whether the admin page has registered settings
+ *
+ * @since 1.0.0
+ *
+ * @param string $page
+ * @return bool Does the admin page have settings?
+ */
+function paco2017_admin_page_has_settings( $page = '' ) {
+
+	// Bail when page is empty
+	if ( empty( $page ) )
+		return false;
+
+	// Loop through the available sections
+	$sections = wp_list_filter( paco2017_admin_get_settings_sections(), array( 'page' => $page ) );
+	foreach ( (array) $sections as $section_id => $section ) {
+
+		// Find out whether the section has fields
+		$fields = paco2017_admin_get_settings_fields_for_section( $section_id );
+		if ( ! empty( $fields ) ) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/** Main Section **********************************************************/
+
+/**
+ * Main settings section description for the settings page
+ *
+ * @since 1.0.0
+ */
+function paco2017_admin_setting_callback_main_section() { /* Nothing to display */ }
+
 /** Pages ***************************************************************/
 
 /**
  * Output the contents of the main admin page
  *
  * @since 1.0.0
+ *
+ * @uses do_action() Calls 'paco2017_admin_page-{$page}'
  */
-function paco2017_admin_page() {
+function paco2017_admin_page() { ?>
+
+	<div class="wrap">
+
+		<h1><?php esc_html_e( 'Paascongres 2017', 'paco2017-content' ); ?></h1>
+
+		<h2 class="nav-tab-wrapper"><?php paco2017_admin_page_tabs(); ?></h2>
+
+		<?php do_action( 'paco2017_admin_page-' . paco2017_admin_page_get_current_page() ); ?>
+
+	</div>
+
+	<?php
 
 }
 
@@ -26,6 +137,98 @@ function paco2017_admin_page() {
  *
  * @since 1.0.0
  */
-function paco2017_admin_settings_page() {
+function paco2017_admin_settings_page() { ?>
 
+	<form action="options.php" method="post">
+
+		<?php settings_fields( 'paco2017' ); ?>
+
+		<?php do_settings_sections( 'paco2017' ); ?>
+
+		<?php submit_button(); ?>
+
+	</form>
+
+	<?php
+}
+
+/**
+ * Display the admin settings page tabs items
+ *
+ * @since 1.0.0
+ */
+function paco2017_admin_page_tabs() {
+
+	// Get the admin pages
+	$pages = paco2017_admin_page_get_pages();
+	$page  = paco2017_admin_page_get_current_page();
+
+	// Walk registered pages
+	foreach ( $pages as $slug => $label ) {
+
+		// Skip empty pages
+		if ( empty( $label ) )
+			continue;
+
+		// Print the tab item
+		printf( '<a class="nav-tab%s" href="%s">%s</a>',
+			( $page === $slug ) ? ' nav-tab-active' : '',
+			esc_url( add_query_arg( array( 'page' => $slug ), admin_url( 'admin.php' ) ) ),
+			$label
+		);
+	}
+}
+
+/**
+ * Return the admin page pages
+ *
+ * @since 0.0.7
+ *
+ * @uses apply_filters() Calls 'paco2017_admin_page_get_pages'
+ * @return array Tabs as $page-slug => $label
+ */
+function paco2017_admin_page_get_pages() {
+
+	// Setup return value
+	$pages = array(
+		'paco2017' => esc_html__( 'Main', 'paco2017-content' )
+	);
+
+	// Add the settings page
+	if ( paco2017_admin_page_has_settings( 'paco2017' ) ) {
+		$pages['paco2017-settings'] = esc_html__( 'Settings', 'paco2017-content' );
+	}
+
+	return (array) apply_filters( 'paco2017_admin_page_get_pages', $pages );
+}
+
+/**
+ * Return whether any admin page pages are registered
+ *
+ * @since 1.0.0
+ *
+ * @return bool Haz admin page pages?
+ */
+function paco2017_admin_page_has_pages() {
+	return (bool) paco2017_admin_page_get_pages();
+}
+
+/**
+ * Return the current admin page
+ *
+ * @since 1.0.0
+ *
+ * @return string The current admin page. Defaults to the first page.
+ */
+function paco2017_admin_page_get_current_page() {
+
+	$pages = array_keys( paco2017_admin_page_get_pages() );
+	$page  = ( isset( $_GET['page'] ) && in_array( $_GET['page'], $pages ) ) ? $_GET['page'] : false;
+
+	// Default to the first page
+	if ( ! $page && $pages ) {
+		$page = reset( $pages );
+	}
+
+	return $page;
 }
