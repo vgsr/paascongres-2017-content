0a57f05a8e58e916a2e79e2d55a050e5e4afb1df
Laurens Offereins <lmoffereins@gmail.com>
2016-12-09 16:19:54 +0100

    BuddyPress: enable listing associated members in admin users list

--- includes/extend/buddypress/admin.php
+++ includes/extend/buddypress/admin.php
@@ -35,6 +35,7 @@ class Paco2017_BuddyPress_Admin {
 	private function setup_actions() {
 		add_filter( 'manage_users_columns',       array( $this, 'users_add_columns'   )        );
 		add_filter( 'manage_users_custom_column', array( $this, 'users_custom_column' ), 10, 3 );
+		add_action( 'pre_user_query',             array( $this, 'pre_user_query'      )        );
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 	}
@@ -80,7 +81,7 @@ class Paco2017_BuddyPress_Admin {
 			$pos = array_search( 'role', array_keys( $columns ) );
 
 			// Insert before the 'Role' column
-			$columns = array_slice( $columns, 0, $pos -1 ) + array(
+			$columns = array_slice( $columns, 0, $pos ) + array(
 				'paco2017_association' => esc_html__( 'Association', 'paco2017-content' )
 			) + array_slice( $columns, $pos );
 		}
@@ -104,11 +105,48 @@ class Paco2017_BuddyPress_Admin {
 			$assoc = paco2017_bp_xprofile_get_association_value( $user_id );
 
 			if ( ! empty( $assoc ) ) {
-				$content .= $assoc;
+				$url = add_query_arg( array( 'paco2017-association' => urlencode( $assoc ) ) );
+				$content .= '<a href="' . esc_url( $url ) . '">' . esc_html( $assoc ) . '</a>';
 			}
 		}
 
 		return $content;
+	}
+
+	/**
+	 * Modify the admin's user query
+	 *
+	 * @since 1.0.0
+	 *
+	 * @global WPDB   $wpdb
+	 * @global string $pagenow
+	 *
+	 * @param WP_User_Query $user_query
+	 */
+	public function pre_user_query( $user_query ) {
+		global $wpdb, $pagenow;
+
+		// Filter by Association
+		if ( is_admin() && 'users.php' === $pagenow && ! empty( $_REQUEST['paco2017-association'] ) ) {
+
+			// Get the associated field
+			if ( ! $field = paco2017_bp_xprofile_get_association_field() )
+				return;
+
+			// Setup profile query
+			$profile_query = new BP_XProfile_Query( array(
+				array(
+					'field'   => $field->id,
+					'value'   => urldecode( $_REQUEST['paco2017-association'] ),
+					'compare' => '='
+				)
+			) );
+			$profile_clauses = $profile_query->get_sql( $wpdb->users, 'ID' );
+
+			// Append clauses
+			$user_query->query_from  .= $profile_clauses['join'];
+			$user_query->query_where .= $profile_clauses['where'];
+		}
 	}
 }
 
