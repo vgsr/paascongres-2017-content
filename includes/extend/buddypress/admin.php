ac7bd8261d89c0d04d1b75e7a68ec0dca8ca8e18
Laurens Offereins <lmoffereins@gmail.com>
2017-01-04 22:13:25 +0100

    BuddyPress: add profile field admin metabox with setting to mark fields as required for enrollment completion

--- includes/extend/buddypress/admin.php
+++ includes/extend/buddypress/admin.php
@@ -37,6 +37,8 @@ class Paco2017_BuddyPress_Admin {
 
 		// XProfile
 		add_action( 'xprofile_admin_field_name_legend', array( $this, 'xprofile_field_admin_label' ), 99 );
+		add_action( 'xprofile_field_before_submitbox',  array( $this, 'xprofile_details_metabox'   )     );
+		add_action( 'xprofile_fields_saved_field',      array( $this, 'xprofile_save_metabox'      )     );
 	}
 
 	/** Public methods **************************************************/
@@ -86,6 +88,68 @@ class Paco2017_BuddyPress_Admin {
 		// Association field
 		} elseif ( paco2017_bp_xprofile_is_association_field( $field ) ) {
 			printf( $wrap, esc_html_x( 'Association Field', 'admin field label', 'paco2017-content' ) );
+		}
+	}
+
+	/**
+	 * Output the profile field Details metabox
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param BP_XProfile_Field $field
+	 */
+	public function xprofile_details_metabox( $field ) {
+
+		// Bail when this is the primary field
+		if ( 1 === $field->id )
+			return;
+
+		?>
+
+		<div id="paco2017-details-div" class="postbox">
+			<h2><?php _e( 'Paascongres Details', 'paco2017-content' ); ?></h2>
+			<div class="inside">
+				<div>
+					<ul>
+						<li>
+							<label for="paco2017-enrollment-complete">
+								<input type="checkbox" id="paco2017-enrollment-complete" name="paco2017-enrollment-complete" value="1" <?php checked( bp_xprofile_get_meta( $field->id, 'field', 'paco2017_enrollment_complete' ) ); ?> />
+								<?php esc_html_e( 'Require field for enrollment completion', 'buddypress' ); ?>
+							</label>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<input type="hidden" name="paco2017-field-details" value="1" />
+		</div>
+
+		<?php
+	}
+
+	/**
+	 * Save the contents of the profile field Details metabox
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param BP_XProfile_Field $field
+	 */
+	public function xprofile_save_metabox( $field ) {
+
+		// Bail when the metabox was not submitted
+		if ( ! isset( $_POST['paco2017-field-details'] ) )
+			return;
+
+		// Walk field activity options
+		foreach ( array(
+			'paco2017-enrollment-complete' => 'paco2017_enrollment_complete',
+		) as $option => $meta_key ) {
+
+			// Define meta value
+			$value = (int) ( isset( $_POST[ $option ] ) && $_POST[ $option ] );
+
+			// Update field meta
+			bp_xprofile_update_meta( $field->id, 'field', $meta_key, $value );
 		}
 	}
 }
