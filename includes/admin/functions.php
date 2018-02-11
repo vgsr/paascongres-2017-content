3ef99f9a4181ea2aaa3b4bd046d2738c123f7da7
Laurens Offereins <lmoffereins@gmail.com>
2018-02-11 17:22:46 +0100

    Admin: move admin page logic to admin/functions.php

--- includes/admin/functions.php
+++ includes/admin/functions.php
@@ -145,6 +145,134 @@ function paco2017_admin_submenu_taxonomy( $taxonomy = '', $function = '' ) {
 	);
 }
 
+/** Pages ***************************************************************/
+
+/**
+ * Output the contents of the main admin page
+ *
+ * @since 1.0.0
+ *
+ * @uses do_action() Calls 'paco2017_admin_page-{$page}'
+ */
+function paco2017_admin_page() { ?>
+
+	<div class="wrap">
+
+		<h1><?php esc_html_e( 'Paascongres', 'paco2017-content' ); ?></h1>
+
+		<h2 class="nav-tab-wrapper"><?php paco2017_admin_page_tabs(); ?></h2>
+
+		<?php do_action( 'paco2017_admin_page-' . paco2017_admin_page_get_current_page() ); ?>
+
+	</div>
+
+	<?php
+
+}
+
+/**
+ * Display the admin settings page tabs items
+ *
+ * @since 1.0.0
+ */
+function paco2017_admin_page_tabs() {
+
+	// Get the admin pages
+	$pages = paco2017_admin_page_get_pages();
+	$page  = paco2017_admin_page_get_current_page();
+
+	// Walk registered pages
+	foreach ( $pages as $slug => $label ) {
+
+		// Skip empty pages
+		if ( empty( $label ) )
+			continue;
+
+		// Print the tab item
+		printf( '<a class="nav-tab%s" href="%s">%s</a>',
+			( $page === $slug ) ? ' nav-tab-active' : '',
+			esc_url( add_query_arg( array( 'page' => $slug ), admin_url( 'admin.php' ) ) ),
+			$label
+		);
+	}
+}
+
+/**
+ * Return the admin page pages
+ *
+ * @since 1.0.0
+ *
+ * @uses apply_filters() Calls 'paco2017_admin_page_get_pages'
+ * @return array Tabs as $page-slug => $label
+ */
+function paco2017_admin_page_get_pages() {
+
+	// Setup return value
+	$pages = array(
+		'paco2017' => esc_html__( 'Dashboard', 'paco2017-content' )
+	);
+
+	// Main settings page
+	if ( paco2017_admin_page_has_settings( 'paco2017' ) ) {
+		$pages['paco2017-settings'] = esc_html__( 'Settings', 'paco2017-content' );
+	}
+
+	// Partners settings page
+	if ( paco2017_admin_page_has_settings( 'paco2017-partners' ) ) {
+		$pages['paco2017-partners'] = esc_html_x( 'Partners', 'settings tab title', 'paco2017-content' );
+	}
+
+	// Accounts admin tab
+	if ( paco2017_admin_is_current_page( 'paco2017-accounts' ) && current_user_can( 'paco2017_manage_accounts' ) ) {
+		$pages['paco2017-accounts'] = esc_html_x( 'Accounts', 'settings tab title', 'paco2017-content' );
+	}
+
+	return (array) apply_filters( 'paco2017_admin_page_get_pages', $pages );
+}
+
+/**
+ * Return whether any admin page pages are registered
+ *
+ * @since 1.0.0
+ *
+ * @return bool Haz admin page pages?
+ */
+function paco2017_admin_page_has_pages() {
+	return (bool) paco2017_admin_page_get_pages();
+}
+
+/**
+ * Return the current admin page
+ *
+ * @since 1.0.0
+ *
+ * @return string The current admin page. Defaults to the first page.
+ */
+function paco2017_admin_page_get_current_page() {
+
+	$pages = array_keys( paco2017_admin_page_get_pages() );
+	$page  = ( isset( $_REQUEST['page'] ) && in_array( $_REQUEST['page'], $pages ) ) ? $_REQUEST['page'] : false;
+
+	// Default to the first page
+	if ( ! $page && $pages ) {
+		$page = reset( $pages );
+	}
+
+	return $page;
+}
+
+/**
+ * Return whether this is the current page
+ *
+ * @since 1.1.0
+ *
+ * @param  string $page Page slug
+ * @return bool Is this the current page?
+ */
+function paco2017_admin_is_current_page( $page ) {
+	return isset( $_REQUEST['page'] ) && $page === $_REQUEST['page'];
+}
+
 /** Metaboxes ***********************************************************/
 
 /**
